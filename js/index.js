3e34e171b8b507368dcbcd87c3e38a4fe9cb2737
nayaritvp <nayaritvp@gmail.com>
2023-09-04 15:02:36 -0300

    modificacion correcciones

--- js/index.js
+++ js/index.js
@@ -1,24 +1,7 @@
-const cardsContent = document.getElementById('contenedorevents');
+const eventos = data.events
+const fecha = data.currentDate;
+const creartarjeta = createCard;
 
-function createCard(events) {
-  const card = document.createElement("div");
-  card.classList.add('col-12','col-sm-6','col-md-4','col-xl-3')
-  card.innerHTML = `
-  <div class="card h-100 d-flex text-center text-bg-secondary mb-3 my-3">
-  <img src="${events.image}" class="card-img-top" alt="${events.name}">
-  <div class="card-body">
-      <h5 class="card-title">${events.name}</h5>
-      <h5 class="card-title">${events.date}</h5>
-      <p class="card-text">${events.description}</p>
-      <p>$${events.price}</p>
-      <a href="#" class="btn btn-light">details</a>
-  </div>`;
-
-  cardsContent.appendChild(card);
+for (let eventos of data.events) {
+  creartarjeta(eventos);
 }
-
-window.onload = () => {
-  for (const events of data.events) {
-      createCard(events);
-  }
-};
--- js/upcoming_events.js
+++ js/upcoming_events.js
@@ -1,27 +1,9 @@
-const cardsContent = document.querySelector("#contenedorevents");
+const eventos = data.events
+const fecha = data.currentDate;
+const creartarjeta = createCard;
 
-function createCard(events) {
-  const card = document.createElement("div");
-  card.classList.add('col-12','col-sm-6','col-md-4','col-xl-3')
-  card.innerHTML = `
-  <div class="card h-100 d-flex text-center text-bg-secondary mb-3">
-  <img src="${events.image}" class="card-img-top" alt="${events.name}">
-  <div class="card-body">
-      <h5 class="card-title">${events.name}</h5>
-      <h5 class="card-title">${events.date}</h5>
-      <p class="card-text">${events.description}</p>
-      <p>$${events.price}</p>
-      <a href="#" class="btn btn-light">details</a>
-  </div>`;
-
-  cardsContent.appendChild(card);
+for (let eventos of data.events) {
+  if (eventos.date > data.currentDate) {
+    creartarjeta(eventos);
+  }
 }
-
-window.onload = () => {
-    const currentDate = data.currentDate;
-    for (const events of data.events) {
-        if (new Date(events.date) > new Date(currentDate)) {
-            createCard(events);
-        }
-    }
-};
