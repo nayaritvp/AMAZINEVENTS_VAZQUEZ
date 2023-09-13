61f625d16bc54e63581cc8af83f94a3e3ef3f989
nayaritvp <nayaritvp@gmail.com>
2023-09-12 23:55:00 -0300

    modificaciones de tablas faltantes

--- js/stats.js
+++ js/stats.js
@@ -45,27 +45,31 @@ function pintarprimeratab(){
 let data2 = [];
 
 let datostabla2 = [];
+let datostabla3 = [];
+let fechaactual
 const tabla2 = document.querySelector('#tabla2');
+const tabla3 = document.querySelector('#tabla3');
 traerDatos2();
-
 
 function traerDatos2() {
   fetch('https://mindhub-xj03.onrender.com/api/amazing')
     .then(response => response.json())
     .then(dataeventos => {
       data2 = dataeventos;
-      console.log(data2.events);
-      extraderdatos2tabla(data2.events);
+      fechaactual = dataeventos.currentDate
+      extraerdatos2tabla(data2.events);
       pintarestadisticas2();
+      extraerdatos3tabla(data2.events);
+      pintarestadisticas3();
     })
     .catch(error => {
       console.error('Error al cargar los datos:', error);
     });
 }
 
-function extraderdatos2tabla(datos) {
+function extraerdatos2tabla(datos) {
   let categorias = [...new Set(datos.map(elemento => elemento.category))];
-  console.table(categorias);
+
   categorias.forEach(categoria => {
     let fila = {
       categoria: categoria,
@@ -76,31 +80,30 @@ function extraderdatos2tabla(datos) {
     let multipcat = 0;
     let totalAsistencia = 0;
     let totalCapacidad = 0;
-    
-    let datosporcategoria = datos.filter(elemento => elemento.category === categoria);
-    datosporcategoria.forEach(elemento => {
-      if (!isNaN(elemento.assistance) && !isNaN(elemento.price)) {
-        multipcat += +elemento.assistance * elemento.price;
+    let datosporcategoria = datos.filter(elemento => elemento.date <= fechaactual && elemento.category == categoria);
+    datosporcategoria.forEach(elemento => { 
+      if (elemento.assistance!=undefined) {
+        multipcat += elemento.assistance * elemento.price;
       }
       
       if (!isNaN(elemento.assistance) && !isNaN(elemento.capacity)) {
-        totalAsistencia += +elemento.assistance;
-        totalCapacidad += +elemento.capacity;
+        totalAsistencia += elemento.assistance;
+        totalCapacidad += elemento.capacity;
       }
     });
     fila.revenues = multipcat;
+    fila.revenues = Intl.NumberFormat("es-CL").format(fila.revenues);
 
     if (totalCapacidad > 0) {
       fila.porcentajeasistencia = (totalAsistencia / totalCapacidad) * 100;
     } else {
-      fila.porcentajeasistencia = 0; // Evitar la división por cero
+      fila.porcentajeasistencia = 0; 
     }
+    fila.porcentajeasistencia = fila.porcentajeasistencia.toFixed(2)
 
     datostabla2.push(fila);
   });
 }
-
-
 
 function pintarestadisticas2() {
   let html = '';
@@ -108,9 +111,59 @@ function pintarestadisticas2() {
     html += `<tr>
       <td>${fila.categoria}</td>
       <td>${fila.revenues}</td>
-      <td>${fila.porcentajeasistencia}</td>
+      <td>${fila.porcentajeasistencia}%</td>
       </tr>`;
   });
   tabla2.innerHTML = html;
 }
 
+
+function extraerdatos3tabla(datos) {
+  let categorias = [...new Set(datos.map(elemento => elemento.category))];
+
+  categorias.forEach(categoria => {
+    let fila = {
+      categoria: categoria,
+      revenues: 0,
+      porcentajeasistencia: 0,
+    };
+
+    let multipcat = 0;
+    let totalAsistencia = 0;
+    let totalCapacidad = 0;
+    let datosporcategoria = datos.filter(elemento => elemento.date > fechaactual && elemento.category == categoria);
+    datosporcategoria.forEach(elemento => { 
+      if (elemento.estimate!=undefined) {
+        multipcat += elemento.estimate * elemento.price;
+      }
+      
+      if (!isNaN(elemento.estimate) && !isNaN(elemento.capacity)) {
+        totalAsistencia += elemento.estimate;
+        totalCapacidad += elemento.capacity;
+      }
+    });
+    fila.revenues = multipcat;
+    fila.revenues = Intl.NumberFormat("es-CL").format(fila.revenues);
+
+    if (totalCapacidad > 0) {
+      fila.porcentajeasistencia = (totalAsistencia / totalCapacidad) * 100;
+    } else {
+      fila.porcentajeasistencia = 0; 
+    }
+    fila.porcentajeasistencia = fila.porcentajeasistencia.toFixed(2)
+
+    datostabla3.push(fila);
+  });
+}
+
+function pintarestadisticas3() {
+  let html = '';
+  datostabla3.forEach(fila => {
+    html += `<tr>
+      <td>${fila.categoria}</td>
+      <td>${fila.revenues}</td>
+      <td>${fila.porcentajeasistencia}%</td>
+      </tr>`;
+  });
+  tabla3.innerHTML = html;
+}
